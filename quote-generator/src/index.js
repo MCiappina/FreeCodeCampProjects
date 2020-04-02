38ac6d3f4f62c190913113cfeff5cea69c7f2d2a
MCiappina <mciappops@gmail.com>
2020-04-01 22:37:17 -0300

    -

--- quote-generator/src/index.js
+++ quote-generator/src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const quote = ['Lorem ipsum dolor sit amet',
+'consectetur adipiscing elit.',
+'Nam non neque at tortor varius eleifend sit amet ut sem.',
+'Vivamus id venenatis justo, non commodo ante'
+];
+
+function Display (props){
+return (
+<div>
+<div>
+<p style={{color: props.color, backgroundColor: 'white'}}>{props.quote}</p>
+</div>
+</div>
+);
+}
+
+class Buttons extends React.Component {
+constructor(props){
+super(props);
+this.handleQuote = this.handleQuote.bind(this);
+}
+
+handleQuote(){
+this.props.onClick();
+}
+
+render(){
+return (
+<button onClick={this.handleQuote}> New quote </button>
+);
+}
+}
+
+class App extends React.Component {
+constructor(props){
+super(props);
+this.state = {
+quote: "i'm a placeholder",
+color: 'rgb(66, 209, 245)',
+}
+this.newQuote = this.newQuote.bind(this);
+}
+
+newQuote(){
+this.setState({ quote: quote[2]})
+}
+
+render (){
+return (
+<div style={{backgroundColor: this.state.color}}>
+<Display
+quote={this.state.quote}
+color={this.state.color}
+/>
+<Buttons onClick={this.newQuote}/>
+</div>
+);
+}
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<App />,
+document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
